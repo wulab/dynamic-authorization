a577b82e082c995afcdcb43df9ff7934752763ca
Weera Wu <ing@oozou.com>
2020-02-12 12:37:24 +0700

    Initial commit

---
+++ spec/lib/game_spec.rb
@@ -0,0 +1,11 @@
+require './lib/game'
+
+RSpec.describe Game do
+  describe "#score" do
+    it "returns 0 for an all gutter game" do
+      game = Game.new
+      20.times { game.roll(0) }
+      expect(game.score).to eq(0)
+    end
+  end
+end
